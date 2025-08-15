1737663542a31958d0d821b9230bf2317e8cdb59
Jayaprakash <jp008882@gmail.com>
2025-08-15 22:28:38 +0530

    feat: Add success and error message alerts to the layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,6 +67,20 @@
                     </div>
                 @endforeach
             @endif
+
+            @if (session()->has('success'))
+                <div class="bg-green-100 border border-green-400 text-green-700 px-3 py-2 rounded relative w-72 ms-auto my-1 flex items-center"
+                    role="alert">
+                    <span class="block sm:inline">{{ session('success') }}</span>
+                </div>
+            @endif
+
+            @if (session()->has('error'))
+                <div class="bg-red-100 border border-red-400 text-red-700 px-3 py-2 rounded relative w-72 ms-auto my-1 flex items-center"
+                    role="alert">
+                    <span class="block sm:inline">{{ session('error') }}</span>
+                </div>
+            @endif
         </div>
 
         <!-- Page Heading -->
